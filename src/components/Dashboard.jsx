ac401fadb0daa32bd1e495b460e7a8ce5ec86199
masud2005 <masud20012005@gmail.com>
2025-02-21 22:53:15 +0600

    Updated Functionality Added

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -9,6 +9,8 @@ import Swal from "sweetalert2";
 const Dashboard = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const { user } = useContext(AuthContext);
+    const [selectedTask, setSelectedTask] = useState(null);
+    console.log(selectedTask);
 
     const { data: tasks = [], refetch } = useQuery({
         queryKey: ["tasks", user?.email],
@@ -43,8 +45,6 @@ const Dashboard = () => {
     };
 
     const handleDeleteTask = async (id) => {
-        // if (!window.confirm("Are you sure you want to delete this task?")) return;
-
         Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
@@ -59,11 +59,6 @@ const Dashboard = () => {
                     const res = await axios.delete(`http://localhost:5000/all-task/${id}`);
                     toast.success("Task deleted successfully!");
                     refetch();
-                    // Swal.fire({
-                    //     title: "Deleted!",
-                    //     text: "Your file has been deleted.",
-                    //     icon: "success"
-                    // });
                 } catch (error) {
                     toast.error(`Failed to delete task: ${error.response?.data?.message || error.message}`);
                 }
@@ -79,6 +74,40 @@ const Dashboard = () => {
         //     toast.error(`❌ Failed to delete task: ${error.response?.data?.message || error.message}`);
         // }
     };
+
+    const handleUpdateTask = (task) => {
+        // e.preventDefault();
+        console.log(task._id);
+        setSelectedTask(task);
+
+        setIsModalOpen(true);
+    }
+
+    const updateTask = async (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.title.value;
+        const description = form.description.value;
+        const category = form.category.value;
+
+        const updatedTask = { title, description, category };
+        // console.log(updateTask);
+
+        try {
+            const res = await axios.patch(`http://localhost:5000/all-task/${selectedTask._id}`, updatedTask);
+            console.log(res.data);
+
+            if (res.data.modifiedCount) {
+                refetch();
+                toast.success("Task updated successfully!");
+                setIsModalOpen(false);
+            } else {
+                toast.warn("No changes made!");
+            }
+        } catch (error) {
+            toast.error(`Error updating task: ${error.message}`);
+        }
+    }
 
     return (
         <div className="p-6 max-w-4xl mx-auto">
@@ -113,7 +142,7 @@ const Dashboard = () => {
                                         <div className="flex gap-2">
                                             <button
                                                 className="text-blue-500 hover:text-blue-700"
-                                                onClick={() => console.log("Edit Task", task._id)}
+                                                onClick={() => handleUpdateTask(task)}
                                             >
                                                 <FaEdit size={18} />
                                             </button>
@@ -191,6 +220,69 @@ const Dashboard = () => {
                     </div>
                 </div>
             )}
+
+            {isModalOpen && selectedTask && (
+                <div className="modal modal-open">
+                    <div className="modal-box max-w-[768px] px-3 py-8 relative">
+                        <h2 className="text-xl font-bold mb-4 text-gray-800">Add New Task</h2>
+                        <form onSubmit={updateTask}>
+                            <label>Task Title</label>
+                            <input
+                                type="text"
+                                placeholder="Task Title"
+                                defaultValue={selectedTask.title}
+                                name="title"
+                                className="border p-2 rounded-lg w-full mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                maxLength={50}
+                                required
+                            />
+                            <label>Task Description</label>
+                            <textarea
+                                placeholder="Description"
+                                name="description"
+                                defaultValue={selectedTask.description}
+                                className="border p-2 rounded-lg w-full mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                maxLength={200}
+                                required
+                            />
+                            <label>Task Category</label>
+                            <select
+                                name="category"
+                                className="border p-2 rounded-lg w-full mb-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                                defaultValue={selectedTask.category}
+                            >
+                                {/* <option disabled value="">Select Category</option> */}
+                                <option value="To-Do">To-Do</option>
+                                <option value="In Progress">In Progress</option>
+                                <option value="Done">Done</option>
+                            </select>
+                            <div className="flex justify-end gap-2">
+                                {/* <button
+                                    type="button"
+                                    onClick={() => setIsModalOpen(false)}
+                                    className="bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 transition-colors"
+                                >
+                                    Cancel
+                                </button> */}
+                                <button
+                                    className="btn btn-circle btn-outline fixed top-4 right-4 bg-red-300 hover:bg-red-500"
+                                    onClick={() => setIsModalOpen(false)}
+                                >
+                                    ✕
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition-colors"
+                                >
+                                    Update Task
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
+
         </div>
     );
 };
